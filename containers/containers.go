5e79f98e5b7b1dbc81b5788b08909ae54fa20ecc
Jack Ogina <jackogina60@gmail.com>
2019-11-15 01:04:19 +0300

    added support to docker run via exec, with timeout support

--- containers/containers.go
+++ containers/containers.go
@@ -3,8 +3,10 @@ package containers;
 import (
 	"io"
 	"context"
-	// "bytes"
+	"bytes"
 	"fmt"
+	"time"
+	"os/exec"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/pkg/stdcopy"
@@ -16,32 +18,48 @@ type RunContainerOptions struct{
 	Cmd []string
 	Image string
 	Volumes map[string]struct{}
-	Timeout *int
+	Timeout int
 	Runtime string
 }
 
 type ContainerServiceInterface interface{
+	GetClient() client.Client;
 	BuildDockerImage(dockerFile string, containerName string) (err error);
-	RunContainer(runContainerOptions RunContainerOptions) (stdOut io.Writer, stdErr io.Writer, err error)
+	RunContainer(runContainerOptions RunContainerOptions) (stdOut io.Reader, stdErr io.Reader, err error)
+}
+
+type ExecContainerServiceInterface interface{
+	RunContainer(runContainerOptions RunContainerOptions) (stdOut io.Reader, stdErr io.Reader, err error)
 }
 
 type ContainerService struct{
 	Client *client.Client
 }
 
+type ExecContainerService struct{
+
+}
+
+
+func (s *ContainerService) GetClient() client.Client{
+	return *s.Client;
+}
+
 
 func (s *ContainerService) RunContainer(runContainerOptions RunContainerOptions,
-	)(stdOut io.Writer, stdErr io.Writer, err error){
+	)(stdOut io.Reader, stdErr io.Reader, err error){
 
 	ctx := context.Background();
 	containerConfig := &container.Config{
 		Image:runContainerOptions.Image,
 		Cmd:runContainerOptions.Cmd,
 		Volumes:runContainerOptions.Volumes,
+		NetworkDisabled:true,
+		StopSignal:"SIGTERM",
 
 	}
-	if(runContainerOptions.Timeout != nil){
-		containerConfig.StopTimeout = runContainerOptions.Timeout;
+	if(&runContainerOptions.Timeout != nil){
+		containerConfig.StopTimeout = &runContainerOptions.Timeout;
 	}
 	cnt, err := s.Client.ContainerCreate(
 		ctx,
@@ -55,28 +73,39 @@ func (s *ContainerService) RunContainer(runContainerOptions RunContainerOptions,
 	if(err != nil){
 		return stdOut, stdErr, err;
 	}
-	
+	fmt.Println("reached here");
 	err = s.Client.ContainerStart(
 		ctx, 
 		cnt.ID, 
 		types.ContainerStartOptions{},
 	);
+	
 	if(err != nil){
 		return stdOut, stdErr, err;
 	}
 
-	statusCh, errCh := s.Client.ContainerWait(
-		ctx,
-		cnt.ID,
-		container.WaitConditionNotRunning,
-	)
-	select{
-		case err = <-errCh:
-			if(err != nil){
-				return stdOut, stdErr, err;
-			}
-		case <- statusCh:
+	// statusCh, errCh := s.Client.ContainerWait(
+	// 	ctx,
+	// 	cnt.ID,
+	// 	container.WaitConditionNextExit,
+	// )
+	// select{
+	// 	case err = <-errCh:
+	// 		if(err != nil){
+	// 			return stdOut, stdErr, err;
+	// 		}
+	// 	case <- statusCh:
+	// }
+
+	timeout := time.Second * 5;
+
+	
+	err =  s.Client.ContainerStop(ctx, cnt.ID, &timeout);
+	if(err != nil){
+		return stdOut, stdErr, err;
 	}
+	// fmt.Println("reached here");
+
 	logOptions := types.ContainerLogsOptions{
 		ShowStderr:true,
 		ShowStdout:true,
@@ -90,7 +119,11 @@ func (s *ContainerService) RunContainer(runContainerOptions RunContainerOptions,
 	if(err != nil){
 		return stdOut, stdErr, err;
 	}
-	stdcopy.StdCopy(stdOut, stdErr, logs);
+	logStdOut := new(bytes.Buffer);
+	logStdErr := new(bytes.Buffer);
+	stdcopy.StdCopy(logStdOut, logStdErr, logs);
+	stdOut = bytes.NewReader(logStdOut.Bytes())
+	stdErr = bytes.NewReader(logStdErr.Bytes())
 	return stdOut, stdErr, nil;
 
 }
@@ -100,12 +133,85 @@ func (s ContainerService) BuildDockerImage(dockerFile string,
 		return fmt.Errorf("Not Implemented");
 	}
 
+
+func (s *ExecContainerService) RunContainer(runContainerOptions RunContainerOptions,
+	) (stdOut io.Reader, stdErr io.Reader, err error){
+
+		fmt.Sprintf("docker run --rm --runtime")
+
+		dockerCommand := []string{
+			"run",
+			"--rm",
+		};
+
+		// add runtime
+		if(runContainerOptions.Runtime != ""){
+			dockerCommand = append(dockerCommand, fmt.Sprintf("--runtime=%s",runContainerOptions.Runtime));
+		}
+
+		// volumes
+		for k,v := range runContainerOptions.Volumes{
+			dockerCommand = append(dockerCommand, fmt.Sprintf("-v %s:%s",k,v));
+		}
+
+		// image
+		dockerCommand = append(dockerCommand, runContainerOptions.Image);
+		// command
+		dockerCommand = append(dockerCommand, runContainerOptions.Cmd...);
+		
+		fmt.Println(dockerCommand);
+		cmd := exec.Command("docker", dockerCommand...);
+		logStdOut := new(bytes.Buffer);
+		logStdErr := new(bytes.Buffer);
+		cmd.Stdout = logStdOut;
+		cmd.Stderr = logStdErr;
+
+		// start cmd
+		err = cmd.Start();
+		if(err!=nil){
+			return stdOut, stdErr, err;
+		}
+
+		done :=  make(chan error);
+		go func() { done <- cmd.Wait() }()
+		// timer
+		timeout := time.Duration(runContainerOptions.Timeout) * time.Second;
+		timer := time.After(timeout);
+
+		select{
+			case err = <-done:
+				stdOut = bytes.NewReader(logStdOut.Bytes());
+				stdErr = bytes.NewReader(logStdErr.Bytes());
+				if(err!=nil){
+					return stdOut, stdErr, err;
+				}
+				// stdOut = bytes.NewReader(logStdOut.Bytes());
+				// stdErr = bytes.NewReader(logStdErr.Bytes());
+
+			case <-timer:
+				err = cmd.Process.Kill();
+				if(err!=nil){
+					return stdOut, stdErr, err;
+				}
+				stdOut = bytes.NewReader([]byte(""));
+				stdErr =  bytes.NewReader([]byte("\nError: Process Timed out\n"));
+		}
+		return stdOut, stdErr, err;
+	}
+
 func NewContainerService() (*ContainerService, error){
-	s := &ContainerService{};
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation());
-	if(err != nil){
+		s := &ContainerService{};
+		cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation());
+		if(err != nil){
+			return s,err;
+		}
+		s.Client = cli;
 		return s,err;
 	}
-	s.Client = cli;
-	return s,err;
-}
+
+func NewExecContainerService() (*ExecContainerService, error){
+		s := &ExecContainerService{};
+		return s,nil;
+	}
+	
+	
