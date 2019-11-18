9051f920e5067a13f3f6532183c2db7de7ee0bed
Jack Ogina <jackogina60@gmail.com>
2019-11-19 01:35:50 +0300

    init branch

--- sandbox/config.go
+++ sandbox/config.go
@@ -2,5 +2,9 @@ package sandbox;
 
 
 type Config struct{
+	Language string `json:"language"`
+	SourceCode string `json:"source_code"`
+	Strategy string `json:"strategy"`
+	TestCode string
 	
 }
--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -5,21 +5,30 @@ import (
 	"fmt"
 )
 
-
 type Opt struct{
 	Strategy string `json:"strategy" validate:`
 }
 
 func (opt *Opt) OK() (err error){
 	validStrategies := []string{"run","test"};
-	isValidStrategy
+	
 }
 
+func (opt *Opt) ValidateStrategy() (err error){
+	isValidStrategy :=  false;
+	validStrategies := []string{"run","test"};
+	for _,strategy :=  range validStrategies{
+		if strategy == opt.Strategy {
+			isValidStrategy = true;
+		}
+	}
+}
 
 type SandBoxInterface interface{
 	Run() io.Reader
 }
 
+// SandBox executes code in sa
 type SandBox struct{
 	Opt Opt
 }
