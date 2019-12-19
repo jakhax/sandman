9b2146b816e842242325e980993f33a2f208fbca
Jack Ogina <jackogina60@gmail.com>
2019-12-19 09:41:49 +0300

    implemented runner Test method

--- runner/python/python.go
+++ runner/python/python.go
@@ -2,6 +2,10 @@ package python
 import (
 	"github.com/jakhax/sandman/runneropt"
 	"io"
+	"os"
+	"fmt"
+
+	"io/ioutil"
 	"github.com/jakhax/sandman/spawn"
 )
 
@@ -22,6 +26,64 @@ func (r *Runner) Files(opt *runneropt.Opt)(err error){
 
 //TestIntegration method
 func (r *Runner) TestIntegration(opt *runneropt.Opt)(stdout,stderr io.Reader, err error){
+	return r.Python3Unittest(opt)
+}
+
+// Python3Unittest method
+// .
+// |-- setup.py
+// |-- solution.py
+// |-- codewars_testwrapper.py
+// `-- test
+//     |-- __init__.py
+//     |-- __main__.py
+//     `-- test_solution.py
+// inspired by http://stackoverflow.com/a/27630375
+func (r *Runner) Python3Unittest(opt *runneropt.Opt)(stdout,stderr io.Reader, err error){
+	if len(opt.SetupCode) != 0{
+		errX := ioutil.WriteFile("setup.py",opt.SetupCode,0644)
+		if errX != nil{
+			err = errX
+			return
+		}
+		code := "from solution import *\n"+string(opt.Code)
+		opt.Code = []byte(code)
+	}
+	err = ioutil.WriteFile("solution.py",opt.Code,0644)
+	if err != nil{
+		return
+	}
+	err = os.Mkdir("test",0777)
+	if err != nil{
+		return
+	}
+	err = ioutil.WriteFile("test/__init__.py",[]byte(""),0644)
+	if err != nil{
+		return
+	}
+	testLoader := fmt.Sprint(`import unittest
+import timeout_decorator
+from codewars_unittest import CodewarsTestRunner
+def load_tests(loader, tests, pattern):
+	return loader.discover('.')
+unittest.main(testRunner=CodewarsTestRunner())
+GLOBAL_TIMEOUT=3
+timeout_decorator.timeout(GLOBAL_TIMEOUT)(unittest.main)(testRunner=CodewarsTestRunner())`)
+
+	err = ioutil.WriteFile("test/__main__.py",[]byte(testLoader),0644);
+	if err != nil{
+		return
+	}
+	err = ioutil.WriteFile("test/test_solution.py",opt.Fixture,0644)
+	if err != nil{
+		return
+	}
+	spawnOpt := &spawn.SpawnOpt{
+		Dir:opt.Dir,
+		Timeout:opt.Timeout,
+	};
+	var stdin io.Reader
+	stdout, stderr, err = spawn.Spwan(spawnOpt,"python3",[]string{"test"},stdin)
 	return
 }
 
